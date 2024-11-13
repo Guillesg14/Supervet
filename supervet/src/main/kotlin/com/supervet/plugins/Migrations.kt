706d9eb1270bce91efc782dc4e6bd251af55ea4a
guillesg14 <guillesg14@gmail.com>
2024-11-13 20:26:37 +0100

    fixed migrations.kt

--- supervet/src/main/kotlin/com/supervet/plugins/Migrations.kt
+++ supervet/src/main/kotlin/com/supervet/plugins/Migrations.kt
@@ -6,8 +6,14 @@ import io.ktor.server.application.*
 import org.flywaydb.core.Flyway
 
 fun Application.configureMigrations() {
+    val host = environment.config.property("database.host").getString()
+    val port = environment.config.property("database.port").getString()
+    val database = environment.config.property("database.database").getString()
+    val user = environment.config.property("database.user").getString()
+    val password = environment.config.property("database.password").getString()
+
     val hikariConfig = HikariConfig().apply {
-        jdbcUrl = environment.config.property("database.connectionString").getString()
+        jdbcUrl = "jdbc:postgresql://${host}:${port}/${database}?user=${user}&password=${password}"
         driverClassName = "org.postgresql.Driver"
     }
 
